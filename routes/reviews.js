b592166f00444a08c74bbf618ee6901041727889
davishka-hikkaduwa <sanjaya.hikkaduwa@outlook.com>
2024-02-18 18:00:59 +0100

    refactor: routes

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,8 +3,6 @@ const router = express.Router({ mergeParams: true });
 const catchAsync = require('../utils/catchAsync');
 const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware')
 const reviews = require('../controllers/reviews');
-const Campground = require('../models/campground');
-const Review = require('../models/review');
 
 router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview));
 
