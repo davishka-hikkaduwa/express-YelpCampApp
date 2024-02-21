0ad416f8d1e0b8b3ab02eac5afd87dc34f3afecf
davishka-hikkaduwa <sanjaya.hikkaduwa@outlook.com>
2024-02-21 22:19:12 +0100

    implement mongo-sanitize

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
+	require('dotenv').config();
 }
 
 console.log(process.env.SECRET);
@@ -17,43 +17,48 @@ const app = express();
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
-const { log } = require('console');
 
-main().catch(err => {
-    console.log('Connection failed!');
-    console.log(err);
+main().catch((err) => {
+	console.log('Connection failed!');
+	console.log(err);
 });
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
-    console.log('MONGO Connection successful!');
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+	await mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+	console.log('MONGO Connection successful!');
+	// use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => { 
-    console.log('Database connected') 
+db.once('open', () => {
+	console.log('Database connected');
 });
 
 app.engine('ejs', ejsMate);
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+	mongoSanitize({
+		replaceWith: '_',
+	})
+);
 
 const sessionConfig = {
-    secret: 'developmentsecret',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
+	secret: 'developmentsecret',
+	resave: false,
+	saveUninitialized: true,
+	cookie: {
+		httpOnly: true,
+		expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+		maxAge: 1000 * 60 * 60 * 24 * 7,
+	},
 };
 app.use(session(sessionConfig));
 app.use(flash());
@@ -69,17 +74,17 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use((req, res, next) => {
-    console.log(req.session);
-    res.locals.currentUser = req.user;
-    res.locals.success = req.flash('success');
-    res.locals.error = req.flash('error');
-    next();
+	console.log(req.session);
+	res.locals.currentUser = req.user;
+	res.locals.success = req.flash('success');
+	res.locals.error = req.flash('error');
+	next();
 });
 
 app.get('/fakeUser', async (req, res) => {
-    const user = new User({ email: 'fakemail@mail.com', username: 'faker' });
-    const newUser = await User.register(user, 'faker123456');
-    res.send(newUser);
+	const user = new User({ email: 'fakemail@mail.com', username: 'faker' });
+	const newUser = await User.register(user, 'faker123456');
+	res.send(newUser);
 });
 
 app.use('/', userRoutes);
@@ -87,19 +92,19 @@ app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 app.get('/', (req, res) => {
-    res.render('home');
-})
+	res.render('home');
+});
 
 app.all('*', (req, res, next) => {
-    next(new ExpressError('Page Not Found', 404));
-})
+	next(new ExpressError('Page Not Found', 404));
+});
 
 app.use((err, req, res, next) => {
-    const { statusCode = 500 } = err;
-    if (!err.message) err.message = 'Something went wrong!';
-    res.status(statusCode).render('error', { err });
+	const { statusCode = 500 } = err;
+	if (!err.message) err.message = 'Something went wrong!';
+	res.status(statusCode).render('error', { err });
 });
 
 app.listen(port, () => {
-    console.log(`Serving on ${port}`);
+	console.log(`Serving on ${port}`);
 });
