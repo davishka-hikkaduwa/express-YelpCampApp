937d828484779674177d79aa2eb35732b96d7a85
davishka-hikkaduwa <sanjaya.hikkaduwa@outlook.com>
2024-02-22 22:25:38 +0100

    minor fix

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
 //process.env.DB_URL
-const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp';
+// const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp';
 
 main().catch((err) => {
 	console.log('Connection failed!');
